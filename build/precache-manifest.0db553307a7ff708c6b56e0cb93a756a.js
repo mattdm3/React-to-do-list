ec31c6611deb5d744c550c08a35344b944de9e72
Matteo Mortelliti <mortellitimatteo@gmail.com>
2020-02-12 22:35:44 -0500

    created build

---
+++ build/precache-manifest.0db553307a7ff708c6b56e0cb93a756a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e3fd3b5368991aba6bbac655d71b013",
+    "url": "/index.html"
+  },
+  {
+    "revision": "36890253c73480db8bc1",
+    "url": "/static/js/2.7c882cf2.chunk.js"
+  },
+  {
+    "revision": "809f8196affeaac0e5176b72b79c267c",
+    "url": "/static/js/2.7c882cf2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "960c1208211562b56dac",
+    "url": "/static/js/main.c9bcfa52.chunk.js"
+  },
+  {
+    "revision": "04fa6513b39a9f0e675f",
+    "url": "/static/js/runtime-main.ec143f26.js"
+  }
+]);
